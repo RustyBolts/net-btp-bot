77d8bf25161e5fcc930646cc751d00c45736ece7
Rusty Bolt <xxfox0001@gmail.com>
2025-05-13 15:51:36 +0800

    恢復日誌寫入

--- src/record/SpotLogger.js
+++ src/record/SpotLogger.js
@@ -1,28 +1,28 @@
-// const fs = require('fs');
-// const { Console } = require('console');
+const fs = require('fs');
+const { Console } = require('console');
 
 class SpotLogger {
     constructor() {
         this.logger = {};
     }
 
-    // getFilePath(baseSymbol, quoteSymbol) {
-    //     return `./logs/spot-${baseSymbol}${quoteSymbol}-stdout.log`;
-    // }
+    getFilePath(baseSymbol, quoteSymbol) {
+        return `./logs/spot-${baseSymbol}${quoteSymbol}-stdout.log`;
+    }
 
     // 紀錄 log
     log(baseSymbol, quoteSymbol, ...message) {
-        // const symbol = `${baseSymbol}${quoteSymbol}`;
-        // if (!this.logger[symbol]) {
-        //     const filePath = this.getFilePath(baseSymbol, quoteSymbol);
-        //     const output = fs.createWriteStream(filePath);
-        //     if (!fs.existsSync(filePath)) {
-        //         fs.writeFileSync(filePath, 'first update');
-        //     }
-        //     this.logger[symbol] = new Console({ stdout: output });
-        // }
+        const symbol = `${baseSymbol}${quoteSymbol}`;
+        if (!this.logger[symbol]) {
+            const filePath = this.getFilePath(baseSymbol, quoteSymbol);
+            const output = fs.createWriteStream(filePath);
+            if (!fs.existsSync(filePath)) {
+                fs.writeFileSync(filePath, 'first update');
+            }
+            this.logger[symbol] = new Console({ stdout: output });
+        }
 
-        // this.logger[symbol].log(...message);
+        this.logger[symbol].log(...message);
 
         console.log(...message);
     }
